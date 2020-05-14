2a5a062b2ee9ca05ed1e9ca35aae568d59aac72d
matheustasso <matheustassosc@gmail.com>
2020-05-14 18:52:30 -0300

    Update Contact

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-const menuitems = document.querySelectorAll('.container a')
-
-menuitems.forEach(item => {
-  item.addEventListener('click', scrollToIdOnClick)
-})
-
-function scrollToIdOnClick() {
-  console.log('Clicou')
-}
